76f3788ae2faf5e87fd9feac591c0022b99fbc42
Milo Roche-Vandenbroucque <milo.roche@outlook.com>
2023-04-25 09:51:58 +0200

    Prisma + sql

--- index.js
+++ index.js
@@ -1,87 +1,93 @@
-// Importation des modules nécessaires
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
-
 const { PrismaClient } = require('@prisma/client');
+
 const prisma = new PrismaClient();
 
-// Définition du schéma GraphQL pour les élèves
 const schema = buildSchema(`
-  input StudentInput {
-    nom: String
-    prenom: String
+  type Editor {
+    idEditors: Int!
+    nameEditors: String!
+    games: [Game!]
   }
 
-  type Student {
-    id: ID!
-    nom: String
-    prenom: String
+  type Game {
+    idGames: Int!
+    nameGames: String!
+    editor: Editor!
+  }
+
+  type Stock {
+    idStock: Int!
+    game: Game!
+    store: Store!
+    units: Int!
+    prices: Float
+  }
+
+  type Store {
+    idStores: Int!
+    nameStores: String!
+    stocks: [Stock!]
   }
 
   type Query {
-    getStudent(id: ID!): Student
-    getAllStudents: [Student]
+    allEditors: [Editor!]!
+    allGames: [Game!]!
+    allStocks: [Stock!]!
+    allStores: [Store!]!
   }
 
   type Mutation {
-    createStudent(input: StudentInput): Student
-    updateStudent(id: ID!, input: StudentInput): Student
-    deleteStudent(id: ID!): Student
+    createEditor(nameEditors: String!): Editor!
+    createGame(nameGames: String!, idEditors: Int!): Game!
+    createStore(nameStores: String!): Store!
+    createStock(idGames: Int!, idStores: Int!, units: Int!, prices: Float): Stock!
+    
+    updateEditor(idEditors: Int!, nameEditors: String): Editor!
+    updateGame(idGames: Int!, nameGames: String, idEditors: Int): Game!
+    updateStore(idStores: Int!, nameStores: String): Store!
+    updateStock(idStock: Int!, idGames: Int, idStores: Int, units: Int, prices: Float): Stock!
+    
+    deleteEditor(idEditors: Int!): Boolean!
+    deleteGame(idGames: Int!): Boolean!
+    deleteStore(idStores: Int!): Boolean!
+    deleteStock(idStock: Int!): Boolean!
   }
 `);
 
-// Stockage des élèves
-const students = [];
+const rootValue = {
+  allEditors: () => prisma.editor.findMany(),
+  allGames: () => prisma.game.findMany(),
+  allStocks: () => prisma.stock.findMany(),
+  allStores: () => prisma.store.findMany(),
+  
+  createEditor: ({ nameEditors }) => prisma.editor.create({ data: { nameEditors } }),
+  createGame: ({ nameGames, idEditors }) => prisma.game.create({ data: { nameGames, editor: { connect: { idEditors } } } }),
+  createStore: ({ nameStores }) => prisma.store.create({ data: { nameStores } }),
+  createStock: ({ idGames, idStores, units, prices }) => prisma.stock.create({ data: { game: { connect: { idGames } }, store: { connect: { idStores } }, units, prices } }),
 
-// Fonctions de gestion des requêtes GraphQL
-const root = {
-  // Récupérer un élève par ID
-  getStudent: ({ id }) => students.find((student) => student.id === id),
+  updateEditor: ({ idEditors, nameEditors }) => prisma.editor.update({ where: { idEditors }, data: { nameEditors } }),
+  updateGame: ({ idGames, nameGames, idEditors }) => prisma.game.update({ where: { idGames }, data: { nameGames, editor: idEditors ? { connect: { idEditors } } : undefined } }),
+  updateStore: ({ idStores, nameStores }) => prisma.store.update({ where: { idStores }, data: { nameStores } }),
+  updateStock: ({ idStock, idGames, idStores, units, prices }) => prisma.stock.update({ where: { idStock }, data: { game: idGames ? { connect: { idGames } } : undefined, store: idStores ? { connect: { idStores } } : undefined, units, prices } }),
 
-  // Récupérer tous les élèves
-  getAllStudents: () => students,
+  deleteEditor: ({ idEditors }) => prisma.editor.delete({ where: { idEditors } }).then(() => true).catch(() => false),
+  deleteGame: ({ idGames }) => prisma.game.delete({ where: { idGames } }).then(() => true).catch(() => false),
+  deleteStore: ({ idStores }) => prisma.store.delete({ where: { idStores } }).then(() => true).catch(() => false),
+  deleteStock: ({ idStock }) => prisma.stock.delete({ where: { idStock } }).then(() => true).catch(() => false),
 
-  // Créer un nouvel élève
-  createStudent: ({ input }) => {
-    const id = Date.now().toString();
-    const student = { id, ...input };
-    students.push(student);
-    return student;
-  },
-
-  // Mettre à jour un élève existant
-  updateStudent: ({ id, input }) => {
-    const index = students.findIndex((student) => student.id === id);
-    if (index === -1) {
-      throw new Error('Élève introuvable');
-    }
-    const updatedStudent = { ...students[index], ...input };
-    students[index] = updatedStudent;
-    return updatedStudent;
-  },
-
-  // Supprimer un élève
-  deleteStudent: ({ id }) => {
-    const index = students.findIndex((student) => student.id === id);
-    if (index === -1) {
-      throw new Error('Élève introuvable');
-    }
-    const removedStudent = students.splice(index, 1)[0];
-    return removedStudent;
-  },
 };
 
-// Configuration du serveur Express
 const app = express();
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: schema,
-    rootValue: root,
+    schema,
+    rootValue,
     graphiql: true,
-  })
+  }),
 );
 
-// Démarrage du serveur
-app.listen(4000, () => console.log('Serveur en écoute sur http://localhost:4000/graphql'));
+app.listen(4000, () => console.log('🚀 Server ready at http://localhost:4000/graphql'));
